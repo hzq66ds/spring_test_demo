8d1192abbe8295a34f666a36f05ca169a45512d9
韩志强 <hanzhiqiang@ppznet.com>
2019-01-25 17:57:17 +0800

    上传文件

---
+++ demo_01/src/main/java/com/springboot/demo_01/Demo01Application.java
@@ -0,0 +1,26 @@
+package com.springboot.demo_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/hello")
+public class Demo01Application {
+
+	@RequestMapping("/{name}")
+	public String sayHello(@PathVariable("name") String name){
+		return "say hello "+name;
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo01Application.class, args);
+	}
+
+}
+

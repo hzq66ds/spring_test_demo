5f2e6bdac5aff29e16be41b11ea4623fd6ca4a8b
韩志强 <hanzhiqiang@ppznet.com>
2019-01-25 19:17:54 +0800

    新增类

--- demo_01/src/main/java/com/springboot/demo_01/restcontrllers/RestController01.java
+++ demo_01/src/main/java/com/springboot/demo_01/restcontrllers/RestController01.java
@@ -1,4 +1,14 @@
 package com.springboot.demo_01.restcontrllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/RestController01")
 public class RestController01 {
+    @RequestMapping("/test01/{name}/{age}")
+    public String test01(@PathVariable("name")String name,@PathVariable("age")Integer age){
+        return "test01\t"+name+"\t"+age;
+    }
 }

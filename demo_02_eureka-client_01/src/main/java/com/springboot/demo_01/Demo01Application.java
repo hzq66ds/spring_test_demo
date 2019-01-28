57d4f21eeba821b2cf67664df92cf62f0b868b59
韩志强 <hanzhiqiang@ppznet.com>
2019-01-28 09:14:03 +0800

    文件同步

--- demo_02_eureka-client_01/src/main/java/com/springboot/demo_01/Demo01Application.java
+++ demo_02_eureka-client_01/src/main/java/com/springboot/demo_01/Demo01Application.java
@@ -2,6 +2,7 @@ package com.springboot.demo_01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @SpringBootApplication
 @RestController
 @RequestMapping("/hello")
+@EnableEurekaClient
 public class Demo01Application {
 
 	@RequestMapping("/{name}")
--- demo_02_eureka-server/src/main/java/com/springboot/demo_01/Demo01Application.java
+++ demo_02_eureka-server/src/main/java/com/springboot/demo_01/Demo01Application.java
@@ -2,13 +2,14 @@ package com.springboot.demo_01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaServer
 @RequestMapping("/hello")
 public class Demo01Application {
 
